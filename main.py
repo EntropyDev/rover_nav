4e84e1b88cbb894440c36d8b4ed740425f859b46
EntropyDev <vchimu20@gmail.com>
2017-10-27 18:00:06 +0530

    exception handling and edge cases

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ This module finds final co-ordinates and heading for each rover. """
 
 
 import sys
+import re
 
 
 # A list of directions in clockwise order
@@ -26,19 +27,29 @@ def turnRight(pos):
     pos[4] = dir_[(i+1) % 4]  # Change the direction after a right turn
 
 
-def move(pos):
-    # Change the coordinate according to the direction
-    if(pos[4] == 'N'):
-        pos[2] += 1
-    elif(pos[4] == 'E'):
-        pos[0] += 1
-    elif(pos[4] == 'S'):
-        pos[2] -= 1
-    elif(pos[4] == 'W'):
-        pos[0] -= 1
+def move(pos, toprightX, toprightY):
+    try:
+        if(pos[0] < 0 or pos[2] < 0 or
+           pos[0] > toprightX or
+           pos[2] > toprightY):
+                raise ValueError('Rover co-ordinates on edge !!')
+    except ValueError as err:  # Edge case exception handling
+        print(err.args)
+        sys.exit(1)
+    else:
+        # Change the coordinate according to the direction
+        if(pos[4] == 'N'):
+            pos[2] += 1
+        elif(pos[4] == 'E'):
+            pos[0] += 1
+        elif(pos[4] == 'S'):
+            pos[2] -= 1
+        elif(pos[4] == 'W'):
+            pos[0] -= 1
 
 
-def rover(pos, nav_inst_list):
+def rover(pos, nav_inst_list, toprightX, toprightY):
+    # For each instruction call respective methods
     while(nav_inst_list):
         inst = nav_inst_list.pop(0)
         if(inst == 'L'):
@@ -46,32 +57,60 @@ def rover(pos, nav_inst_list):
         elif(inst == 'R'):
             turnRight(pos)
         elif(inst == 'M'):
-            move(pos)
+            move(pos, int(toprightX), int(toprightY))
     res = ''.join(str(e) for e in pos)
-    print(res)
+    print(res)  # Print the final coordinate
 
 
 def main():
     # Get input from file
     filename = sys.argv[1]
-    f = open(filename, 'rU')
-    # Setting the top-right coordinates
-    toprightX = f.readline(1)
-    toprightY = f.readline(2)
-    # Reading the rest of the lines
-    for line in f:
-        txt = f.read()
-    f.close()
-    all_inst = txt.split('\n')
-    for i in range(0, len(all_inst), 2):
-        # Converting strings into lists
-        pos = list(all_inst[i])
-        nav_inst_list = list(all_inst[i+1])
-        # Converting the coordinates in pos to integers
-        pos[0] = int(pos[0])
-        pos[2] = int(pos[2])
-        rover(pos, nav_inst_list)  # Calling the rover for execution
+    try:
+        f = open(filename, 'rU')
+    except FileNotFoundError:
+        print('File not found Error.')
+    else:
+        # Setting the top-right coordinates
+        toprightX = f.readline(1)
+        toprightY = f.readline(2)
+        try:
+            m = re.match("(^\d\s\d)", toprightX+toprightY)
+            if not m:
+                raise ValueError('The top line should be coordinates')
+        except ValueError as err:
+            print(err.args)
+        else:
+            # Reading the rest of the lines
+            for line in f:
+                txt = f.read()
+            f.close()
+            all_inst = txt.split('\n')
 
+            for i in range(0, len(all_inst), 2):
+                # Converting strings into lists
+                pos = list(all_inst[i])
+                nav_inst_list = list(all_inst[i+1])
+                try:
+                    m = re.match("(^\d\d[N,E,S,W])", all_inst[i][0]
+                                 + all_inst[i][2]
+                                 + all_inst[i][4])
+                    if not m:
+                        raise ValueError('The initial co-ordinates'
+                                         'error')
+                except ValueError as err:
+                    print(err.args)
+                try:
+                    m = re.match("(^[L,R,M]+)", all_inst[i+1])
+                    if(m.group() != all_inst[i+1]):
+                        raise ValueError('The instructions error')
+                except ValueError as err:
+                    print(err.args)
+                else:
+                    # Converting the coordinates in pos to integers
+                    pos[0] = int(pos[0])
+                    pos[2] = int(pos[2])
+                    # Calling the rover for execution
+                    rover(pos, nav_inst_list, toprightY, toprightY)
 
 # Standard boilerplate to call the main() function to begin
 # the program
