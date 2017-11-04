3910dbeb2adeacb2268eaff994cdf420a1eaa2fe
EntropyDev <vchimu20@gmail.com>
2017-11-04 13:59:44 +0530

    object oriented

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #! Python3 - PEP8 Style Guide -tt
-""" A squad of robotic rovers are to be landed by NASA on a plateau on
+"""
+A squad of robotic rovers are to be landed by NASA on a plateau on
 Mars. This plateau, which is curiously rectangular, must be navigated
 by the rovers so that their on-board cameras can get a complete view of
 the surrounding terrain to send back to Earth.
@@ -7,7 +8,8 @@ the surrounding terrain to send back to Earth.
 This module finds final co-ordinates and heading for each rover.
 
 Author : Vaibhav Chimalgi
-Created : October 2017 """
+Created : October 2017
+"""
 
 
 import sys
@@ -19,48 +21,71 @@ import os
 dir_ = ['N', 'E', 'S', 'W']
 
 
-def turnLeft(cur_dir):
-    # Find the position of current direction from dir_
-    i = dir_.index(cur_dir)
-    return dir_[i-1]  # Return the direction after a left turn
+class Rover:
+    """
+    This Class operates on the rover's position. The class methods
+    allow the rover to turn left, right and move.
+    """
+
+    def turnLeft(self, cur_dir):
+        """
+        Makes a left turn from the current direction of the rover's
+        heading and returns the new heading.
+        """
+        global dir_
+        # Find the position of current direction from dir_
+        i = dir_.index(cur_dir)
+        return dir_[i-1]  # Return direction after a left turn
+
+    def turnRight(self, cur_dir):
+        """
+        Makes a right turn from the current direction of the rover's
+        heading and returns the new heading.
+        """
+        global dir_
+        # Find the position of current direction from dir_
+        i = dir_.index(cur_dir)
+        return dir_[(i+1) % 4]  # Return direction after a right turn
+
+    def move(self, pos, toprightX, toprightY):
+        """
+        Moves one unit distance from the current position in the
+        direction of rover's heading.
+        """
+        # Change the coordinate according to the direction
+        if(pos[4] == 'N' and pos[2] < toprightY):
+            pos[2] += 1
+            return pos[2]
+        elif(pos[4] == 'E' and pos[0] < toprightX):
+            pos[0] += 1
+            return pos[0]
+        elif(pos[4] == 'S' and pos[2] > 0):
+            pos[2] -= 1
+            return pos[2]
+        elif(pos[4] == 'W' and pos[0] > 0):
+            pos[0] -= 1
+            return pos[0]
+        else:
+            raise ValueError('Rover co-ordinates are on the '
+                             'edge !!. Cannot move off the edge')
 
 
-def turnRight(cur_dir):
-    # Find the position of current direction from dir_
-    i = dir_.index(cur_dir)
-    return dir_[(i+1) % 4]  # Return the direction after a right turn
-
-
-def move(pos, toprightX, toprightY):
-    # Change the coordinate according to the direction
-    if(pos[4] == 'N' and pos[2] < toprightY):
-        pos[2] += 1
-        return pos[2]
-    elif(pos[4] == 'E' and pos[0] < toprightX):
-        pos[0] += 1
-        return pos[0]
-    elif(pos[4] == 'S' and pos[2] > 0):
-        pos[2] -= 1
-        return pos[2]
-    elif(pos[4] == 'W' and pos[0] > 0):
-        pos[0] -= 1
-        return pos[0]
-    else:
-        raise ValueError('Rover co-ordinates are on the '
-                         'edge !!. Cannot move off the edge')
-
-
-def rover(pos, nav_inst_list, toprightX, toprightY):
+def navigate(pos, nav_inst_list, toprightX, toprightY):
+    """
+    Executes all the instructions for the current rover and returns the
+    final position and heading of the rover.
+    """
     # For each instruction call respective methods
+    rover = Rover()
     while(nav_inst_list):
         try:
             inst = nav_inst_list.pop(0)
             if(inst == 'L'):
-                pos[4] = turnLeft(pos[4])
+                pos[4] = rover.turnLeft(pos[4])
             elif(inst == 'R'):
-                pos[4] = turnRight(pos[4])
+                pos[4] = rover.turnRight(pos[4])
             elif(inst == 'M'):
-                move(pos, int(toprightX), int(toprightY))
+                rover.move(pos, int(toprightX), int(toprightY))
         except ValueError as err:
             print(err.args)
             return
@@ -69,9 +94,13 @@ def rover(pos, nav_inst_list, toprightX, toprightY):
 
 
 def main(filename):
+    """
+    Takes the input from the filename provided as command line argument
+    and prints the final co-ordinates and heading for each rover.
+    """
     # Get input from file
     try:
-        f = open(os.getcwd()+"/"+filename, 'rU')
+        f = open(filename, 'rU')
     except FileNotFoundError:
         print('File not found Error.')
     else:
@@ -122,8 +151,8 @@ def main(filename):
                     pos[0] = int(pos[0])
                     pos[2] = int(pos[2])
                     # Calling the rover method for execution
-                    res = rover(pos, nav_inst_list,
-                                toprightY, toprightY)
+                    res = navigate(pos, nav_inst_list,
+                                   toprightY, toprightY)
                     print(res)
 
 
--- test.py
+++ test.py
@@ -4,27 +4,30 @@ from main import *
 
 
 class TestCaseTurn(unittest.TestCase):
+    def setUp(self):
+        self.rover = Rover()
 
     def test_turning_left(self):
-        assert turnLeft('N') == 'W'
-        assert turnLeft('E') == 'N'
-        assert turnLeft('S') == 'E'
-        assert turnLeft('W') == 'S'
+        assert self.rover.turnLeft('N') == 'W'
+        assert self.rover.turnLeft('E') == 'N'
+        assert self.rover.turnLeft('S') == 'E'
+        assert self.rover.turnLeft('W') == 'S'
 
     def test_turning_right(self):
-        assert turnRight('N') == 'E'
-        assert turnRight('E') == 'S'
-        assert turnRight('S') == 'W'
-        assert turnRight('W') == 'N'
+        assert self.rover.turnRight('N') == 'E'
+        assert self.rover.turnRight('E') == 'S'
+        assert self.rover.turnRight('S') == 'W'
+        assert self.rover.turnRight('W') == 'N'
 
 
 class TestCaseMove(unittest.TestCase):
 
     def setUp(self):
-        self.moveEast = move([2, ' ', 4, ' ', 'E'], 7, 7)
-        self.moveWest = move([5, ' ', 3, ' ', 'W'], 7, 7)
-        self.moveNorth = move([3, ' ', 5, ' ', 'N'], 7, 7)
-        self.moveSouth = move([5, ' ', 4, ' ', 'S'], 7, 7)
+        self.rover = Rover()
+        self.moveEast = self.rover.move([2, ' ', 4, ' ', 'E'], 7, 7)
+        self.moveWest = self.rover.move([5, ' ', 3, ' ', 'W'], 7, 7)
+        self.moveNorth = self.rover.move([3, ' ', 5, ' ', 'N'], 7, 7)
+        self.moveSouth = self.rover.move([5, ' ', 4, ' ', 'S'], 7, 7)
 
     def test_move(self):
         self.assertIn(self.moveEast, range(1, 8))
@@ -34,25 +37,25 @@ class TestCaseMove(unittest.TestCase):
 
     def test_move_off_edge(self):
         with self.assertRaises(ValueError):
-            move([4, ' ', 6, ' ', 'N'], 5, 5)
+            self.rover.move([4, ' ', 6, ' ', 'N'], 5, 5)
         with self.assertRaises(ValueError):
-            move([4, ' ', 6, ' ', 'E'], 3, 5)
+            self.rover.move([4, ' ', 6, ' ', 'E'], 3, 5)
         with self.assertRaises(ValueError):
-            move([-1, ' ', 6, ' ', 'W'], 5, 5)
+            self.rover.move([-1, ' ', 6, ' ', 'W'], 5, 5)
         with self.assertRaises(ValueError):
-            move([4, ' ', -1, ' ', 'S'], 5, 5)
+            self.rover.move([4, ' ', -1, ' ', 'S'], 5, 5)
 
 
 class TestRover(unittest.TestCase):
 
     def test_rover(self):
-        assert rover([5, ' ', 4, ' ', 'N'],
-                     ['L', 'M', 'L', 'M', 'M', 'R', 'R', 'M'],
-                     7, 8) == '4 3 N'
+        assert navigate([5, ' ', 4, ' ', 'N'],
+                        ['L', 'M', 'L', 'M', 'M', 'R', 'R', 'M'],
+                        7, 8) == '4 3 N'
 
     def test_rover_inst(self):
-        assert rover([2, ' ', 4, ' ', 'N'],
-                     ['M', 'M', 'M'], 3, 5) is None
+        assert navigate([2, ' ', 4, ' ', 'N'],
+                        ['M', 'M', 'M'], 3, 5) is None
 
 
 class TestMain(unittest.TestCase):
